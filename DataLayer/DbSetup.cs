d3629e790270dc6e37661c481d4f3ede9be86253
barakis <lokislayer@gmail.com>
2026-01-14 18:09:01 -0600

    Begin Setting up the DB Backend.

---
+++ DataLayer/DbSetup.cs
@@ -0,0 +1,16 @@
+﻿namespace DataLayer
+{
+    public abstract class DbSetup
+    {
+        /*
+         * DataSource => SQLITE | SQLSERVER {EITHER CASE IT IS A FILE}
+         * DbName => FlashCards
+         * 3 Tables {Card / Stck / Study} -- Many Cards to 1 Stack Relation 
+         * 1 VIEW which is virtual table of Stgack and Card Data mostly for viewing
+         * Scripts for the following:
+         * Creation of all tables. All will be blank except Stack which record 1 is DEFAULT
+         * Reading the data / Update Data / Deleting Data              
+         */
+        protected readonly string _dbName = "FlashCards";
+    }
+}
