0f1d7ba53357bad0f2c7997628e787969d8852c0
Matthew Brown <lokislayer@gmail.com>
2026-01-19 01:27:16 -0600

    DataType based of my appsettings json file.

---
+++ DataLayer/Models/DbConfig.cs
@@ -0,0 +1,10 @@
+namespace DataLayer.Models;
+
+public class DbConfig
+{
+    public string DbName { get; set; } = string.Empty;
+    public string StackTable { get; set; } = string.Empty;
+    public string CardTable { get; set; } = string.Empty;
+    public string StudiesTable { get; set; } = string.Empty;
+    public string NotImplementedMsg { get; set; } = string.Empty;
+}
