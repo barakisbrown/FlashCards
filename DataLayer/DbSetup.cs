ba398bb37c41520d9ee17226427dd70c53b14e5a
Matthew Brown <lokislayer@gmail.com>
2026-01-19 01:26:29 -0600

    Ready to begin the actual setup of the DB if needed.

--- DataLayer/DbSetup.cs
+++ DataLayer/DbSetup.cs
@@ -7,20 +7,15 @@ using Microsoft.Extensions.Configuration;
     * DataSource => SQLITE | SQLSERVER {EITHER CASE IT IS A FILE}
     * DbName => FlashCards
     * 3 Tables {Card / Stck / Study} -- Many Cards to 1 Stack Relation 
-    * 1 VIEW which is virtual table of Stgack and Card Data mostly for viewing
+    * 1 VIEW which is virtual table of Stack and Card Data mostly for viewing
     * Scripts for the following:
     * Creation of all tables. All will be blank except Stack which record 1 is DEFAULT
     * Reading the data / Update Data / Deleting Data              
  */
 public class DbSetup
 {
-
     public DbSetup()
     {
 
     }
-    public string DbName { get; set; }
-    public string StackTable { get; set; }
-    public string CardTable { get; set; }
-    public string StudiesTable { get; set; }
 }
