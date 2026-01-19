acf17544b231f0dfbdd292d23f466ef970fe2a9a
barakis <lokislayer@gmail.com>
2026-01-19 17:43:35 -0600

    Updated model for the scripts

--- DataLayer/Models/DbConfig.cs
+++ DataLayer/Models/DbConfig.cs
@@ -7,4 +7,9 @@ public class DbConfig
     public string CardTable { get; set; } = string.Empty;
     public string StudiesTable { get; set; } = string.Empty;
     public string NotImplementedMsg { get; set; } = string.Empty;
+
+    // SQL FILES BELOW
+    public string CreateDBSql { get; set; } = string.Empty;
+    public string CreateCardSql { get; set; } = string.Empty;
+    public string CreateStackSql { get; set; } = string.Empty;
 }
