b164b99c8eede35fbd26e962393fb01ff9343f11
barakis <lokislayer@gmail.com>
2026-01-24 00:32:08 -0600

    Added Controller Folder and two classes

---
+++ DataLayer/Controller/CardController.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer.Models;
+
+namespace DataLayer.Controller
+{
+    internal static class CardController
+    {
+        private static readonly DbConfig appSettings = Configuration.LoadSettings();
+        private static readonly string connectionStringMain = Configuration.GetConnectionStrings(appSettings.MainConn);
+
+        public static bool CreateCard(Card card)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool UpdatedCard(Card card)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool DeleteCard(Card card, int id) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Card> GetAllCards() 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataLayer/Controller/StackController.cs
@@ -0,0 +1,35 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Controller
+{    
+    internal static class StackController
+    {
+        private static readonly DbConfig appSettings = Configuration.LoadSettings();
+        private static readonly string connectionStringMain = Configuration.GetConnectionStrings(appSettings.MainConn);
+
+        public static bool CreateStack(Stack stack)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool UpdatedStack(Stack stack)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool DeleteStack(Stack stack)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Stack> GetAllStacks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
