729a9a50c788ce6cf999bcc3dd7ba3811bd2afda
barakis <lokislayer@gmail.com>
2026-02-13 01:26:27 -0600

    Updated to match DB.

--- DataLayer/Models/Card.cs
+++ DataLayer/Models/Card.cs
@@ -3,9 +3,15 @@
     /// <summary>
     /// FlashCard Entity
     /// </summary>
-    /// <param name="CardID">ID OF THE CARD</param>
-    /// <param name="Front">INFO ON FRONT</param>
-    /// <param name="Back">INFO ON BACK</param>
+    /// <param name="ID">ID OF THE CARD</param>
+    /// <param name="Prompt">The question this flashcard asks</param>
+    /// <param name="Answer">Answer the Prompt is asking for</param>
     /// <param name="StackID">ID to the stack this card belongs too</param>
-    public record class Card(int CardID,string? Front,string? Back,int? StackID);
+    public class Card()
+    {
+        public int ID { get; set; }
+        public string Prompt { get; set; } = string.Empty;
+        public string Answer { get; set; } = string.Empty;
+        public int StackID { get; set; }
+    }
 }
