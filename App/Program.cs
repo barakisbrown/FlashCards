68bc00a1ebb39f82d5ec697512e565dd3571c4b7
barakis <lokislayer@gmail.com>
2026-02-13 01:25:36 -0600

    TestBed

--- App/Program.cs
+++ App/Program.cs
@@ -1,7 +1,18 @@
 ﻿using DataLayer;
+using DataLayer.Controller;
+using DataLayer.Models;
 using Spectre.Console;
 
 var db = new DbSetup();
+
+var table = new CardController();
+var card = table.GetCardByID(6);
+
+Console.WriteLine($"There are {table.Count} records in the card table");
+bool success = table.DeleteCard(card);
+Console.WriteLine($"Was record deleted? {success}");
+Console.WriteLine($"There are {table.Count} records in the card table");
+
 
 AnsiConsole.WriteLine("Begin UI Application Here");
 Console.ReadKey();
