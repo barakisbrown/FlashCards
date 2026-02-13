3b794f500e32b627b0513ef3bd562c4dd929f9f8
barakis <lokislayer@gmail.com>
2026-02-13 02:49:47 -0600

    UI PROJECT for doing UI ONLY

--- App/Program.cs
+++ App/Program.cs
@@ -1,7 +1,16 @@
 ﻿using DataLayer;
+using DataLayer.Controller;
 using Spectre.Console;
+using UI;
 
 var db = new DbSetup();
 
+var cardTable = new CardController();
+var stackTable = new StackController();
+
 AnsiConsole.WriteLine("Begin UI Application Here");
+var menu = new Menu(cardTable, stackTable);
+menu.DisplayMenu();
+
+AnsiConsole.WriteLine("Thank you for using the FlashCards App.  Have a good day.");
 Console.ReadKey();
