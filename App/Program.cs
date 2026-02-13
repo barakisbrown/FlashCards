31ee20c586348684d26fd23d071f9c7707aec23d
barakis <lokislayer@gmail.com>
2026-02-13 02:32:10 -0600

    TESTBED

--- App/Program.cs
+++ App/Program.cs
@@ -6,11 +6,7 @@ using Spectre.Console;
 var db = new DbSetup();
 
 var table = new CardController();
-var card = table.GetCardByID(6);
 
-Console.WriteLine($"There are {table.Count} records in the card table");
-bool success = table.DeleteCard(card);
-Console.WriteLine($"Was record deleted? {success}");
 Console.WriteLine($"There are {table.Count} records in the card table");
 
 
