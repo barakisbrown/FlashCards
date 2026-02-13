1fcd2c423292d34de769f7c7ae20b339fc49bc98
barakis <lokislayer@gmail.com>
2026-02-13 02:32:47 -0600

    Completed StackController for now

--- DataLayer/Controller/StackController.cs
+++ DataLayer/Controller/StackController.cs
@@ -1,30 +1,106 @@
-﻿using DataLayer.Models;
+﻿using Dapper;
+using DataLayer.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
+using Spectre.Console;
+using System.Data.Common;
 
 namespace DataLayer.Controller
 {    
-    internal static class StackController
+    /// <summary>
+    /// MATT
+    /// FIRST CREATE A PARAM OBJECT
+    /// Use MakConnection and then Execute except QUERYS
+    /// </summary>
+    public class StackController
     {
-        private static readonly DbConfig appSettings = Configuration.LoadSettings();
-        // FIX CONNECTION STRING ISSUES
+        private static readonly DbUser dataSource = Configuration.GetUserSecretsConnStrings();
+        private readonly string selectSql = "SELECT * FROM dbo.Stack";
+        private readonly string insertSql = "INSERT INTO dbo.Stack(Name) VALUE (@Name)";
+        private readonly string editSQL = "UPDATE dbo.Stack WHERE dbo.Stack.Name = @Name";
+        private readonly string deleteSQL = "DELETE dbo.Stack WHERE dbo.Stack.Name = @Name";
+        private List<Stack> stacks = [];
 
-        public static bool CreateStack(Stack stack)
+        public StackController()
         {
-            throw new NotImplementedException();
+            stacks = GetAllStacks();
         }
 
-        public static bool UpdatedStack(Stack stack)
+        public bool AddStack(string? Name)
         {
-            throw new NotImplementedException();
+            if (Name.IsNullOrEmpty() || VeriyName(Name)) return false;
+            object[] param = { new { Name } };
+            bool success = MakeConnection.Execute(insertSql, param) == 1;
+            if (success)
+            {
+                stacks = GetAllStacks();
+                return true;
+            }
+
+            return false;
         }
 
-        public static bool DeleteStack(Stack stack)
+        public bool EditStack(string? NameEdit)
         {
-            throw new NotImplementedException();
+            if (NameEdit.IsNullOrEmpty()) return false;
+
+            if (VeriyName(NameEdit)) return false;
+
+            if (MakeConnection.Execute(editSQL, new { Name = NameEdit }) == 1)
+            {
+                stacks = GetAllStacks();
+                return true;
+            }
+
+            return false;
+        }
+        
+        public bool DeleteStack(string? deleteMe)
+        {
+            if (deleteMe.IsNullOrEmpty() || deleteMe == "DEFAULT") return false;
+            return MakeConnection.Execute(deleteSQL, new { Name = deleteMe }) == 1;
         }
 
-        public static List<Stack> GetAllStacks()
+
+        public List<Stack> GetAllStacks()
         {
-            throw new NotImplementedException();
+            return MakeConnection.Query<Stack>(selectSql).ToList();
+
+        }
+
+        /// <summary>
+        /// Searches the internal list for a specified name
+        /// </summary>
+        /// <param name="Name">Name to be found</param>
+        /// <returns>true if found false if not found</returns>
+        public bool VeriyName(string ?Name)
+        {
+            var exist = stacks.FirstOrDefault(x => x.Name == Name);
+            return (exist == null);
+
+        }
+
+
+
+        public int COUNT => stacks.Count;
+        private SqlConnection MakeConnection
+        {
+            get
+            {
+                var conn = new SqlConnection(dataSource.Main);
+                if (conn.State != System.Data.ConnectionState.Open)
+                    try
+                    {
+                        conn.Open();
+                    }
+                    catch (Exception e)
+                    {
+                        AnsiConsole.WriteLine("Error problem opening connection to the database engine. CHeck to see if it running.");
+                        throw;
+                    }
+                return conn;
+            }
         }
     }
 }
