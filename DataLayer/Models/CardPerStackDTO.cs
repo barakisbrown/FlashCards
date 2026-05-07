9cdf4cb0d3421e658b7ed120f7627abc52410492
barakis <lokislayer@gmail.com>
2026-05-07 16:33:09 -0500

    Changed Name to TotalCards

--- DataLayer/Models/CardPerStackDTO.cs
+++ DataLayer/Models/CardPerStackDTO.cs
@@ -3,6 +3,6 @@
     public class CardsPerStackDTO
     {             
         public string Name { get; set; }
-        public int NumCards { get; set; }
+        public int TotalCards { get; set; }
     }
 }
