ccad5a380abc3c7c7e482b242caba51731e96f20
barakis <lokislayer@gmail.com>
2026-07-22 18:12:50 -0500

    Model of 1 Study Session

---
+++ DataLayer/Models/Session.cs
@@ -0,0 +1,15 @@
+﻿namespace DataLayer.Models;
+/// <summary>
+/// Session POCO Class
+/// </summary>
+/// <param name="ID">ID of the Study Session</param>
+/// <param name="StudyDate">Date/Time the study was done</param>
+/// <param name="StudyScore">The Score of the Study Session</param>
+/// <param name="StackID">Foreign Key to the stack table</param>
+public class Session
+{
+    public int ID { get; set; }
+    public DateTime StudyDate { get; set; }
+    public int StudyScore { get; set; }
+    public int StackID { get; set; }
+}
