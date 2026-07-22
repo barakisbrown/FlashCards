218663e867d178da465a9f40eabc675c0e2fcefc
barakis <lokislayer@gmail.com>
2026-07-22 18:12:57 -0500

    Update comments

--- DataLayer/Models/Card.cs
+++ DataLayer/Models/Card.cs
@@ -1,17 +1,16 @@
-﻿namespace DataLayer.Models
+﻿namespace DataLayer.Models;
+
+/// <summary>
+/// FlashCard Entity
+/// </summary>
+/// <param name="ID">ID OF THE CARD</param>
+/// <param name="Prompt">The question this flashcard asks</param>
+/// <param name="Answer">Answer the Prompt is asking for</param>
+/// <param name="StackID">ID to the stack this card belongs too</param>
+public class Card()
 {
-    /// <summary>
-    /// FlashCard Entity
-    /// </summary>
-    /// <param name="ID">ID OF THE CARD</param>
-    /// <param name="Prompt">The question this flashcard asks</param>
-    /// <param name="Answer">Answer the Prompt is asking for</param>
-    /// <param name="StackID">ID to the stack this card belongs too</param>
-    public class Card()
-    {
-        public int ID { get; set; }
-        public string Prompt { get; set; } = string.Empty;
-        public string Answer { get; set; } = string.Empty;
-        public int StackID { get; set; }
-    }
+    public int ID { get; set; }
+    public string Prompt { get; set; } = string.Empty;
+    public string Answer { get; set; } = string.Empty;
+    public int StackID { get; set; }
 }
