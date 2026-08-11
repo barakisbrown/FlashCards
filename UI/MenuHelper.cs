82d869e8f21b088272b3378f104ea95e7428413d
barakis <lokislayer@gmail.com>
2026-08-10 19:04:06 -0500

    Change where the prompts point back to what they define.

--- UI/MenuHelper.cs
+++ UI/MenuHelper.cs
@@ -1,6 +1,7 @@
 ﻿
 using DataLayer.Controller;
 using DataLayer.Models;
+using Microsoft.SqlServer.Management.HadrModel;
 using Spectre.Console;
 
 namespace UI;
@@ -10,17 +11,17 @@ public static class MenuHelper
     private readonly static CardController CardController = new();
     private static StackController _stackController = new();
     
-    public static SelectionPrompt<Stack> GetStackNamePrompt(List<Stack>? stackList)
+    public static Stack GetStackNamePrompt(List<Stack>? stackList)
     {
         const string title = $"  STACK";
         var prompt = new SelectionPrompt<Stack>()
             .Title(title)
             .UseConverter(s => $"[bold]{s.Name}[/]")
             .AddChoices<Stack>(stackList);
-        return prompt;
+        return AnsiConsole.Prompt(prompt);
     }
     
-    public static SelectionPrompt<Card> GetCardNamesPrompt(int stackID)
+    public static Card GetCardNamesPrompt(int stackID)
     {
         var list = CardController.GetAllCardsByStack(stackID);
         list.Add(new DataLayer.Models.Card
@@ -35,15 +36,22 @@ public static class MenuHelper
             .Title(title)
             .UseConverter(s => $"{s.Prompt,-15} {s.Answer}")
             .AddChoices<Card>(list);
-        return prompt;
+        return AnsiConsole.Prompt(prompt);
     }
 
-    public static SelectionPrompt<Stack> GetStackListPrompt(List<Stack> stackList,string title)
+    public static Stack GetStackListPrompt(List<Stack> stackList, string title)
     {
         var prompt = new SelectionPrompt<Stack>()
             .Title(title)
             .UseConverter(s => $"[bold]{s.Name}[/]")
             .AddChoices<Stack>(stackList);
-        return prompt;
+        return AnsiConsole.Prompt(prompt);
     }
+
+    public static string GetTextPrompt(string prompt)
+    {
+        var prmpt = new TextPrompt<string>(prompt)
+                .AllowEmpty();
+        return AnsiConsole.Prompt(prmpt);
+    }   
 }
